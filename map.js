6347fb8df6fc3d7ee8e87c321c37551de2477696
agon <agon@hawaii.edu>
2014-02-19 23:09:21 -1000

    Enhaced visual for the World and USA Map.
    Created containers for each map.
    Position and style of containers and maps.

--- map.js
+++ map.js
@@ -6,27 +6,30 @@ google.load('visualization', '1', {packages: ['geochart']});
 //Function to paint the WorldMap
 function worldMap() {
   var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity'],
-    ['Spain', 700],
-    ['United States', 300],
-    ['Brazil', 400],
-    ['Canada', 500],
-    ['France', 600],
-    ['RU', 700]
+    ['Country', 'Popularity', 'Cuantity'],
+    ['Spain', 700, 122],
+    ['United States', 300, 1223],
+    ['Brazil', 400, 900],
+    ['Canada', 500, 800],
+    ['France', 600, 722],
+    ['RU', 700, 300]
   ]);
-  var geochart = new google.visualization.GeoChart(
+  var World = new google.visualization.GeoChart(
       document.getElementById('Worldmap'));
-  geochart.draw(data, {width: 300, height: 300});
+  World.draw(data, {width: 730, height: 372});
 }
 
 
 //Function to paint the USA map
 function usaMap() {
   var data = google.visualization.arrayToDataTable([
-    ['State',   'Population', 'Area'],
-    ['New York',      2761477,    1285.31],
-    ['Hawaii',     1324110,    181.76],
-    ['California',    959574,     117.27]
+    ['State', 'Popularity', 'Cuantity'],
+    ['Hawaii', 700, 122],
+    ['California', 300, 1223],
+    ['Arizona', 400, 900],
+    ['Texas', 500, 800],
+    ['New Mexico', 600, 722],
+    ['Ontario', 700, 300]
   ]);
 
   var options = {
@@ -35,12 +38,13 @@ function usaMap() {
     enableRegionInteractivity: 'true', 
     resolution: 'provinces',
     colorAxis: {colors: ['green', 'blue']},
+    showLegend: true,
     width: 500,
-    height: 500
+    height: 320
   };
 
-  var chart = new google.visualization.GeoChart(document.getElementById('USAmap'));
-  chart.draw(data, options);
+  var USA = new google.visualization.GeoChart(document.getElementById('USAmap'));
+  USA.draw(data, options);
 };
 
 google.setOnLoadCallback(worldMap);

6ac88c9d768db700ebe93b796260f25ca0a3de4b
Andreas Brauchli <a.brauchli@elementarea.net>
2014-02-26 15:41:16 -1000

    Use actual data on world map

--- map.js
+++ map.js
@@ -5,18 +5,15 @@ google.load('visualization', '1', {packages: ['geochart']});
 
 //Function to paint the WorldMap
 function worldMap() {
-  var data = google.visualization.arrayToDataTable([
-    ['Country', 'Popularity', 'Cuantity'],
-    ['Spain', 700, 122],
-    ['United States', 300, 1223],
-    ['Brazil', 400, 900],
-    ['Canada', 500, 800],
-    ['France', 600, 722],
-    ['RU', 700, 300]
-  ]);
   var World = new google.visualization.GeoChart(
       document.getElementById('Worldmap'));
-  World.draw(data, {width: 710, height: 372});
+  var view = new google.visualization.DataView(exportDtaCountry[0]);
+  view.setColumns([1, 2, 3]);
+  World.draw(view, {
+    width: 710,
+    height: 372,
+    colorAxis: { maxValue: 1000 }
+  });
 
   //Function to get the name of the country we click on the world map
   google.visualization.events.addListener(World, 'regionClick', function(eventData)

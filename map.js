18e08d496a3fde8bc37bec4155ced591678c8c0c
Andreas Brauchli <a.brauchli@elementarea.net>
2014-02-27 15:49:57 -1000

    Map filtering on new table schema

--- map.js
+++ map.js
@@ -7,8 +7,27 @@ google.load('visualization', '1', {packages: ['geochart']});
 function worldMap() {
   var World = new google.visualization.GeoChart(
       document.getElementById('Worldmap'));
-  var view = new google.visualization.DataView(exportDtaCountry[0]);
-  view.setColumns([1, 2, 3]);
+  var view = new google.visualization.DataView(stateExportCountries);
+
+  view.setRows(view.getFilteredRows([
+    // Filter out current state w/o the "total" rows
+    {column: 0, value: 'AK'},
+    {column: 1, minValue: 1}
+  ]));
+
+  // Filter out the country (2), year1 and year2
+  view.setColumns([
+    2,
+    3,
+    {
+      label: 'Years',
+      type: 'string',
+      role: 'tooltip',
+      calc: function(t,r) {
+        return 'Col3: '+ t.getValue(r, 3);
+      }
+    }
+  ]);
   World.draw(view, {
     width: 710,
     height: 372,
@@ -26,12 +45,6 @@ function worldMap() {
 
 //Function to paint the USA map
 function usaMap() {
-  var data = google.visualization.arrayToDataTable([
-    ['State', "Description", "2012"],
-    ['Alabama', "ZINC ORES AND CONCENTRATES", 0.3],
-    ['Alaska', "ZINC ORES AND CONCENTRATES", 0.5]
-  ]);
-
   var options = {
     region: 'US',
     displayMode: 'regions',
@@ -44,7 +57,27 @@ function usaMap() {
   };
 
   var USA = new google.visualization.GeoChart(document.getElementById('USAmap'));
-  USA.draw(data, options);
+  var comView = new google.visualization.DataView(stateExportCommodities);
+  /**
+   * Return a list of rows matching an hscode category
+   * @param t the table to search (DataView)
+   * @param c the hscode prefix to match
+   */
+  var hscodeRowFilter = function(t, c) {
+    var i = 0,
+        n = t.getNumberOfRows(),
+        ret = [];
+    for (; i < n; ++i) {
+      if ((''+ t.getValue(i, 2)).substr(0, c.length) === c
+          && t.getValue(i, 1) > 0) {
+        ret.push(i);
+      }
+    }
+    return ret;
+  }
+  comView.setRows(hscodeRowFilter(comView, '26'));
+  comView.setColumns([0, 4 /* 2009 */]);
+  USA.draw(comView, options);
 
   //Function to get the name of the country we click on the USA map
   google.visualization.events.addListener(USA, 'regionClick', function(eventData)

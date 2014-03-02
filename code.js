8c7cd3fce647963457373457dddf6717f92d6f0e
Andreas Brauchli <a.brauchli@elementarea.net>
2014-03-01 14:46:07 -1000

    Trim trailing whitespace, add missing semicolon

--- code.js
+++ code.js
@@ -9,13 +9,13 @@ function main() {
     var select_object = dojo.byId('select');
     var select_div = dojo.byId('msg');
     var selected_values = [];
-    for (x=0;x<=select_object.length;x++) {
+    for (var x=0; x<=select_object.length; x++) {
         if (select_object[x] && select_object[x].selected) {
             selected_values.push(select_object[x].value);
         }
-    }      
+    }
      select_div.innerHTML = 'Selected value = ' + selected_values;
-     console.log(selected_values)                        
+     console.log(selected_values);
     });
 }
 

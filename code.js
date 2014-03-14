6fa67094d732e7913be5b6d68ce7f41de4091aea
agon <agon@hawaii.edu>
2014-03-13 16:49:30 -1000

    Changes to add a selection for the countries

--- code.js
+++ code.js
@@ -51,7 +51,7 @@ function drawRawTable() {
 
 //Function to configure options of chosen-dojo commodities selector and fill in the data to select dynamically
 function initCommoditiesSelector() {
-    //Text to display when hovering the languages in map
+    //Text to fill in the CommoditiesSelector Search bar
     for(var i=0; i<commodities.hscodes.length; ++i) {
         //If it is a group code
         if (commodities.hscodes[i].code == 0) {
@@ -79,10 +79,10 @@ function initCommoditiesSelector() {
             group_code[g] = group_code[g] + '<option>' + commodities.hscodes[i].group + ' ' + commodities.hscodes[i].commodity + '<option>';
         }
     }
-    //console.log(group_code);
-    var contentString = '<div class="side-by-side clearfix"><div><label for="comselect">Commodities</label><select id="comselect"  data-placeholder="Search or select individual or group commodities" style="width:475px;" class="chzn-select-batch" multiple tabindex="6"><option value=""></option>'+ group_code.join('') +'</optgroup></select></div></div>';
+
+    var contentStringCommodities = '<div class="side-by-side clearfix"><div><label for="comselect">Commodities</label><select id="comselect"  data-placeholder="Search or select individual or group commodities" style="width:475px;" class="chzn-select-batch" multiple tabindex="6"><option value=""></option>'+ group_code.join('') +'</optgroup></select></div></div>';
     select = dojo.byId('commodities_selector');
-    select.innerHTML = contentString;
+    select.innerHTML = contentStringCommodities;
 
     //Chosen-dojo options
     dojo.query(".chzn-select").chosen();
@@ -92,27 +92,45 @@ function initCommoditiesSelector() {
     //Management of the selection
     $('comselect').on('change', function(event) {
     var select_object = dojo.byId('comselect');
-    var select_div = dojo.byId('msg');
     var selected_values = [];
     for (var x=0; x<=select_object.length; x++) {
         if (select_object[x] && select_object[x].selected) {
             selected_values.push(select_object[x].value);
         }
     }
-    select_div.innerHTML = 'Selected value = ' + selected_values;
-    console.log(selected_values);
     });
 
     //function removeSelect(selected_values) {
     // What's that ^^ for?
-        $('#comclear')
-        .button()
-        .click(function(e) {
-            e.preventDefault();
-            console.log('clik');
-            selected_values = []
-        });
+    $('#comclear')
+    .button()
+    .click(function(e) {
+        e.preventDefault();
+        console.log('clik');
+        selected_values = []
+    });
     //}
+    //Text to fill in the states Selector for International Imports/Exports USA Map
+    var contentStringStates = '<div class="side-by-side clearfix"><div><label for="stateselect">Commodities</label><select id="stateselect"  data-placeholder="Search or select individual or group commodities" style="width:200px;" class="chzn-select-batch" multiple tabindex="6"><option value=""></option><option>Alaska</option></select></div></div>';
+    select = dojo.byId('state_selector');
+    select.innerHTML = contentStringStates;
+
+    //Chosen-dojo options
+    dojo.query(".chzn-select").chosen();
+    dojo.query(".chzn-select-deselect").chosen({allow_single_deselect:true});
+    dojo.query(".chzn-select-batch").chosen({batch_select:true});
+
+    //Management of the selection
+    $('stateselect').on('change', function(event) {
+    var select_object = dojo.byId('stateselect');
+    var selected_values = [];
+    for (var x=0; x<=select_object.length; x++) {
+        if (select_object[x] && select_object[x].selected) {
+            selected_values.push(select_object[x].value);
+        }
+    }
+    });
+
 }
 
 $(main);
